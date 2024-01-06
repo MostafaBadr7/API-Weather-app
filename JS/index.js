588149728cfc7b930eea44cda2fd224c789b3eda
MostafaBadr7 <mostafa.badr222@yahoo.com>
2024-01-06 23:04:02 +0200

    update dark theme & geolocation function

--- JS/index.js
+++ JS/index.js
@@ -17,20 +17,8 @@ const navMobUl = document.querySelector(".nav-lists-mobile ul");
 const searchBox = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
 const warningBoxMsg = document.querySelector(".error-msg");
-const weatherLocation = document.querySelector(".location");
-const weatherDegree = document.querySelector(".c-js");
-const conditionImg = document.querySelector(".condition-img");
-const weatherStatment = document.querySelector(".weather-status");
-const todayName = document.querySelector(".today-name");
-const currentMonth = document.querySelector(".today-num-month");
-const direction = document.querySelector(".direction");
-const wind = document.querySelector(".wind");
-const rain = document.querySelector(".rain");
-const secondDay = document.querySelector(".second-day");
-const _3rdDay = document.querySelector("._3rd-day");
-const forecastDaysImg = document.querySelectorAll(".forecast-days-img");
-const forecastDaysDeg = document.querySelectorAll(".forecast-days-deg");
-const weatherStatus = document.querySelectorAll(".weather-status");
+const input = document.querySelector("#mode");
+
 //.................................. Search Location weather ...............................//
 searchButton.addEventListener("click", () => {
   if (searchBox != null) {
@@ -46,6 +34,31 @@ searchBox.addEventListener("keypress", (e) => {
     }
   }
 });
+
+//...................................... User's Location .......................................//
+// Check if geolocation is supported by the browser
+if ("geolocation" in navigator) {
+  // Prompt user for permission to access their location
+  navigator.geolocation.getCurrentPosition(
+    // Success callback function
+    (position) => {
+      // Get the user's latitude and longitude coordinates
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      let locationAutomatic = `${lat},${lng}`;
+      // Do something with the location data
+      showWeather(locationAutomatic);
+    },
+    // Error callback function
+    (error) => {
+      // Handle errors, e.g. user denied location sharing permissions
+      console.error("Error getting user location:", error.message);
+    }
+  );
+} else {
+  // Geolocation is not supported by the browser
+  console.error("Geolocation is not supported by this browser.");
+}
 //...................................... Mobile Menu  .......................................//
 
 //open & close mobile screen menu + changing the menu icon
@@ -65,6 +78,23 @@ menuBtn.addEventListener("click", () => {
 //....................................... API Results ..................................//
 async function showWeather(locationName = "cairo") {
   // Local variables to not take memory space globally
+  const weatherLocation = document.querySelector(".location");
+  const weatherDegree = document.querySelector(".c-js");
+  const conditionImg = document.querySelector(".condition-img");
+  const weatherStatment = document.querySelector(".weather-status");
+  const todayName = document.querySelector(".today-name");
+  const currentMonth = document.querySelector(".today-num-month");
+  const direction = document.querySelector(".direction");
+  const wind = document.querySelector(".wind");
+  const rain = document.querySelector(".rain");
+  const secondDay = document.querySelector(".second-day");
+  const _3rdDay = document.querySelector("._3rd-day");
+  const forecastDaysImg = document.querySelectorAll(".forecast-days-img");
+  const forecastDaysDeg = document.querySelectorAll(".forecast-days-deg");
+  const forecastDaysMaxTemp = document.querySelectorAll(".max-temp");
+  const forecastDaysMinTemp = document.querySelectorAll(".min-temp");
+  const todayMinTemp = document.querySelector(".current-min");
+  const weatherStatus = document.querySelectorAll(".weather-status");
   const weekday = [
     "Sunday",
     "Monday",
@@ -100,42 +130,62 @@ async function showWeather(locationName = "cairo") {
     alertMsg("Not exist");
   }
 
-  const d = new Date(`${resp.current.last_updated}`);
+  // Local Variables after getting API response
+  const forecastDays = resp.forecast.forecastday;
+  const currentDay = resp.current;
+  const d = new Date(`${currentDay.last_updated}`);
   const day = weekday[d.getDay()]; // transfer the curent day num to the day's name
   const month = d.getDay() + " " + months[d.getMonth()]; // day's num next to the month's name
-  const secondDayName = new Date(resp.forecast.forecastday[1].date);
-  const _3rdDayName = new Date(resp.forecast.forecastday[2].date);
-
+  const secondDayName = new Date(forecastDays[1].date);
+  const _3rdDayName = new Date(forecastDays[2].date);
+  console.log(currentDay);
+  // Filling in the page the data we got
   weatherLocation.innerHTML = `${resp.location.name}`;
-  weatherDegree.innerHTML = `${resp.current.temp_c}`;
-  weatherStatment.innerHTML = `${resp.current.condition.text}`;
+  weatherDegree.innerHTML = `${currentDay.temp_c}`;
+  todayMinTemp.innerHTML = `min. ${forecastDays[0].day.mintemp_c}`;
+  weatherStatment.innerHTML = `${currentDay.condition.text}`;
   todayName.innerHTML = `${day}`;
   currentMonth.innerHTML = `${month}`;
   // the 3 addons info of the first day ( rain , wind, direction)
-  direction.innerHTML = `${resp.current.wind_dir}`;
-  wind.innerHTML = `${resp.current.wind_kph} km/h`;
-  rain.innerHTML = `${resp.current.precip_in} %`;
+  direction.innerHTML = `${currentDay.wind_dir}`;
+  wind.innerHTML = `${currentDay.wind_kph} km/h`;
+  rain.innerHTML = `${currentDay.precip_in} %`;
   //the second and third Days' name
   secondDay.innerHTML = `${weekday[secondDayName.getDay()]}`;
   _3rdDay.innerHTML = `${weekday[_3rdDayName.getDay()]}`;
 
-  conditionImg.setAttribute("src", `${resp.current.condition.icon}`);
+  conditionImg.setAttribute("src", `${currentDay.condition.icon}`);
 
   //loop on the days started from the second day - because the first day already got its data from the last updated data
   for (let i = 1; i < forecastDaysImg.length + 1; i++) {
     forecastDaysImg[i - 1].setAttribute(
       "src",
-      resp.forecast.forecastday[i].day.condition.icon
+      forecastDays[i].day.condition.icon
     );
     forecastDaysDeg[
       i - 1
-    ].innerHTML = `${resp.forecast.forecastday[i].day.avgtemp_c}<sup>o</sup>C`;
-    weatherStatus[
-      i
-    ].innerHTML = `${resp.forecast.forecastday[i].day.condition.text}`;
+    ].innerHTML = `${forecastDays[i].day.avgtemp_c}<sup>o</sup>C`;
+    forecastDaysMaxTemp[
+      i - 1
+    ].innerHTML = `<span class="min-word">max.</span> ${forecastDays[i].day.maxtemp_c}<sup>o</sup>C`;
+    forecastDaysMinTemp[
+      i - 1
+    ].innerHTML = `<span class="min-word">min.</span> ${forecastDays[i].day.mintemp_c}<sup>o</sup>C`;
+    weatherStatus[i].innerHTML = `${forecastDays[i].day.condition.text}`;
   }
 }
 showWeather();
+//....................................... Dark Theme ..................................//
+if (localTheme == "light") {
+  input.checked = true;
+}
+input.addEventListener("click", () => {
+  const root = document.documentElement;
+  let dataTheme = root.getAttribute("data-theme");
+  let newTheme = dataTheme == "light" ? "dark" : "light";
+  root.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+});
 
 //...................................... Automation Functions ..............................//
 
@@ -148,86 +198,3 @@ function alertMsg(msg) {
     warningBoxMsg.innerHTML = "";
   }, "3000");
 }
-
-// Check if geolocation is supported by the browser
-if ("geolocation" in navigator) {
-  // Prompt user for permission to access their location
-  navigator.geolocation.getCurrentPosition(
-    // Success callback function
-    (position) => {
-      // Get the user's latitude and longitude coordinates
-      const lat = position.coords.latitude;
-      const lng = position.coords.longitude;
-
-      // Do something with the location data, e.g. display on a map
-      console.log(`Latitude: ${lat}, longitude: ${lng}`);
-    },
-    // Error callback function
-    (error) => {
-      // Handle errors, e.g. user denied location sharing permissions
-      console.error("Error getting user location:", error);
-    }
-  );
-} else {
-  // Geolocation is not supported by the browser
-  console.error("Geolocation is not supported by this browser.");
-}
-
-function displayLocation(latitude, longitude) {
-  var request = new XMLHttpRequest();
-
-  var method = "GET";
-  var url =
-    "http://maps.googleapis.com/maps/api/geocode/json?latlng=" +
-    latitude +
-    "," +
-    longitude +
-    "&sensor=true";
-  var async = true;
-
-  request.open(method, url, async);
-  request.onreadystatechange = function () {
-    if (request.readyState == 4 && request.status == 200) {
-      console.log(request);
-
-      var data = JSON.parse(request.responseText);
-      var address = data.results[0];
-      console.log(address);
-    }
-  };
-  request.send();
-}
-
-var successCallback = function (position) {
-  var x = position.coords.latitude;
-  var y = position.coords.longitude;
-  displayLocation(x, y);
-};
-
-var errorCallback = function (error) {
-  var errorMessage = "Unknown error";
-  switch (error.code) {
-    case 1:
-      errorMessage = "Permission denied";
-      break;
-    case 2:
-      errorMessage = "Position unavailable";
-      break;
-    case 3:
-      errorMessage = "Timeout";
-      break;
-  }
-  console.log(errorMessage);
-};
-
-var options = {
-  enableHighAccuracy: true,
-  timeout: 1000,
-  maximumAge: 0,
-};
-
-navigator.geolocation.getCurrentPosition(
-  successCallback,
-  errorCallback,
-  options
-);
